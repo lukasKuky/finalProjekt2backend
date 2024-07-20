f0238a116d59a2f12c0b866f9b563e82ccfbe380
77satyr77 <135996721+77satyr77@users.noreply.github.com>
2024-07-20 10:08:23 +0200

    Add files via upload

---
+++ src/main/java/org/example/final02/Final02Application.java
@@ -0,0 +1,13 @@
+package org.example.final02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Final02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Final02Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/final02/Final02ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.final02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Final02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
