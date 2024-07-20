426b40a764545b9807b450d452016c385fc1a37a
77satyr77 <77satyr77@gmail.com>
2024-07-20 13:12:03 +0200

    skuska gitu

--- src/main/java/org/example/final02/Final02Application.java
+++ src/main/java/org/example/final02/Final02Application.java
@@ -9,6 +9,7 @@ public class Final02Application {
     public static void main(String[] args) {
         SpringApplication.run(Final02Application.class, args);
         System.out.println("aplikacia beží na porte : http://http://localhost:8080/");
+        System.out.println("čau maji");
     }
 
 }
